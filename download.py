b10e667386964ddf63833ca2028472e1eeedb6e7
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-05-26 16:07:30 +0700

    add files downloading

--- download.py
+++ download.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python
+from urllib import request
 
 url = "https://ru-open-stt.ams3.digitaloceanspaces.com"
+
+# download data
+with open("md5sum.lst") as f:
+    for line in f:
+        file = line.split(" ")[1].rstrip()
+        with open(file, "wb+") as data:
+            response = request.urlopen(url + "/" + file)
+            data.write(response.read())
+print("Files was downloaded")
