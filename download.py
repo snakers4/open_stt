3a036d82d80797ca0a9c689a659d09c6cc3225f6
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-05-27 20:47:39 +0700

    Add md5 sum checking

--- download.py
+++ download.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python
+import hashlib
+import os
 from urllib import request
+
 
 url = "https://ru-open-stt.ams3.digitaloceanspaces.com"
 
 # download data
 with open("md5sum.lst") as f:
     for line in f:
-        file = line.split(" ")[1].rstrip()
-        with open(file, "wb+") as data:
-            response = request.urlopen(url + "/" + file)
-            data.write(response.read())
-print("Files was downloaded")
+        md5, file = line.rstrip().split(" ")
+        while True:
+            with open(file, "wb+") as data:
+                response = request.urlopen(url + "/" + file)
+                data.write(response.read())
+            md5_file = hashlib.md5(open(file, "rb").read()).hexdigest()
+            if md5 != md5_file:
+                os.remove(file)
+                print(
+                    "MD5 digest for "
+                    + file
+                    + " is incorrect, the file will be downloaded again."
+                )
+            else:
+                break
+    print("Files was downloaded")
